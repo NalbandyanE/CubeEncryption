bb461ae45ee6a07fde1c181b7da8705b315a90c6
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-06-17 13:51:57 +0400

    Update decoder.cpp

--- cubeEncryption/decoder.cpp
+++ cubeEncryption/decoder.cpp
@@ -23,34 +23,34 @@ std::string Decoder::getKey() const {
 }
 
 void Decoder::decode(Encoder& encoder) {
-    std::vector<std::string> keyParts = encoder.splitString(key, ':');
-    std::string decodedText;
-    for (size_t i = 0; i < keyParts.size(); ++i) {
-        std::vector<std::string> subParts = encoder.splitString(keyParts[i], ',');
-        bool reverseRotation = (i == keyParts.size() - 1);
-        for (size_t j = 0; j < subParts.size(); ++j) {
-            char direction = subParts[j][0];
-            int rotationCount = std::stoi(subParts[j].substr(1));
-            if (direction == 'U') 
-            {
-                encoder.getCubes()[i].rotateDown(rotationCount);
-            } 
-            else if (direction == 'D') {
-                if (reverseRotation) {
-                    encoder.getCubes()[i].rotateUp(rotationCount);
-                } 
-                else {
-                    encoder.getCubes()[i].rotateDown(rotationCount);
-                }
-            } 
-            else if (direction == 'L') {
-                encoder.getCubes()[i].rotateRight(rotationCount);
-            } 
-            else if (direction == 'R') {
-                encoder.getCubes()[i].rotateLeft(rotationCount);
-            }
-        }
-        decodedText += encoder.getCubes()[i].getCube();
-    }
-    setEncodedText(decodedText);
+    	std::vector<std::string> keyParts = encoder.splitString(key, ':');
+    	std::string decodedText;
+    	for (size_t i = 0; i < keyParts.size(); ++i) {
+        	std::vector<std::string> subParts = encoder.splitString(keyParts[i], ',');
+        	bool reverseRotation = (i == keyParts.size() - 1);
+        	for (size_t j = 0; j < subParts.size(); ++j) {
+            		char direction = subParts[j][0];
+            		int rotationCount = std::stoi(subParts[j].substr(1));
+            		if (direction == 'U') 
+            		{
+                		encoder.getCubes()[i].rotateDown(rotationCount);
+            		} 
+            		else if (direction == 'D') {
+                		if (reverseRotation) {
+                    			encoder.getCubes()[i].rotateUp(rotationCount);
+                		} 
+                		else {
+                   	 		encoder.getCubes()[i].rotateDown(rotationCount);
+                		}
+            		} 
+            		else if (direction == 'L') {
+                		encoder.getCubes()[i].rotateRight(rotationCount);
+            		} 
+            		else if (direction == 'R') {
+                		encoder.getCubes()[i].rotateLeft(rotationCount);
+            		}
+        	}
+        	decodedText += encoder.getCubes()[i].getCube();
+    	}
+    	setEncodedText(decodedText);
 }
