7cc856413b756011e1f5d5ecaa8118903b186631
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-06-17 13:53:58 +0400

    Update cube.cpp

--- cubeEncryption/cube.cpp
+++ cubeEncryption/cube.cpp
@@ -13,61 +13,61 @@ std::string Cube::getCube() const {
 }
 
 void Cube::rotateUp(int n) {
-    for (int k = 0; k < n; ++k) {
-    	std::string res = "";
-        res += cube[3];
-        res += cube[2];
-        res += cube[6];
-        res += cube[7];
-        res += cube[0];
-        res += cube[1];
-        res += cube[5];
-        res += cube[4];
-        setCube(res);
-    }
+    	for (int k = 0; k < n; ++k) {
+    		std::string res = "";
+        	res += cube[3];
+        	res += cube[2];
+        	res += cube[6];
+        	res += cube[7];
+        	res += cube[0];
+        	res += cube[1];
+        	res += cube[5];
+        	res += cube[4];
+        	setCube(res);
+    	}
 }
 
 void Cube::rotateDown(int n) {
-    for (int k = 0; k < n; ++k) {
-        std::string res = "";
-        res += cube[4];
-        res += cube[5];
-        res += cube[1];
-        res += cube[0];
-        res += cube[7];
-        res += cube[6];
-        res += cube[2];
-        res += cube[3];
-        setCube(res);
-    }
+    	for (int k = 0; k < n; ++k) {
+        	std::string res = "";
+        	res += cube[4];
+        	res += cube[5];
+        	res += cube[1];
+        	res += cube[0];
+        	res += cube[7];
+        	res += cube[6];
+        	res += cube[2];
+        	res += cube[3];
+        	setCube(res);
+    	}
 }
 
 void Cube::rotateLeft(int n) {
 	for (int k = 0; k < n; ++k) {
-        std::string res = "";
-        res += cube[1];
-        res += cube[5];
-        res += cube[6];
-        res += cube[2];
-        res += cube[0];
-        res += cube[4];
-        res += cube[7];
-        res += cube[3];
-        setCube(res);
-    }
+        	std::string res = "";
+        	res += cube[1];
+        	res += cube[5];
+        	res += cube[6];
+        	res += cube[2];
+        	res += cube[0];
+        	res += cube[4];
+        	res += cube[7];
+        	res += cube[3];
+        	setCube(res);
+    	}
 }
 
 void Cube::rotateRight(int n) {
 	for (int k = 0; k < n; ++k) {
-        std::string res = "";
-        res += cube[4];
-        res += cube[0];
-        res += cube[3];
-        res += cube[7];
-        res += cube[5];
-        res += cube[1];
-        res += cube[2];
-        res += cube[6];
-        setCube(res);
-    }
+        	std::string res = "";
+        	res += cube[4];
+        	res += cube[0];
+        	res += cube[3];
+        	res += cube[7];
+        	res += cube[5];
+        	res += cube[1];
+        	res += cube[2];
+        	res += cube[6];
+        	setCube(res);
+    	}
 }
